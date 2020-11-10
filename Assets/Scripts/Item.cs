41279b731b8e2f73fb871c3b0f26046e5f5d69cb
anavianm <anavian.max@gmail.com>
2020-11-10 02:38:39 -0500

    added base movement and a small following avatar. Started object database

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public int id;
+    public string title;
+    public string description;
+    public Sprite icon;
+    public Dictionary<string, int> stats = new Dictionary<string, int>();
+    
+
+    public Item(int id, string title, string description, Sprite icon, Dictionary<string,int> stats) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.icon = Resources.Load<Sprite>("Sprites/Items/" + title);
+        this.stats = stats;
+    }
+    public Item(Item item) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.icon = Resources.Load<Sprite>("Sprites/Items/" + title);
+        this.stats = stats;
+    }
+}
