5e9f73d04b53a7cfad92a53b646ecd0a73e0baec
anavianm <anavian.max@gmail.com>
2020-11-10 22:51:36 -0500

    file saving

--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class ItemDatabase : MonoBehaviour
 {
+
+    public List<Item> items = new List<Item>();
+
+    void BuildDatabase() {
+        items = new List<Item>() {
+            new Item(0, "Magnifying Glass", "Increases Enemy Warning by 5%", new Dictionary<string, int>{
+                {"Warning", 0.05}
+            }),
+
+            new Item(0, "Friendship bracelet", "Hurts anxiety monster's ability to slow you down by 5%", new Dictionary<string, int>{
+                {"reverseSlow", 0.05}
+            }),
+
+            new Item(0, "Food", "Increases base HP by 5%", new Dictionary<string, int>{
+                {"HP", 0.05}
+            }),
+
+            new Item(0, "Water", "Heals 5% over time", new Dictionary<string, int>{
+                {"Heal", 0.05}
+            }),
+
+            new Item(0, "Friendship bracelet", "Hurts anxiety monster's ability to slow you down by 5%", new Dictionary<string, int>{
+                {"reverseSlow", 0.05}
+            }),
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
