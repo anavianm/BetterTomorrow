a60855202a24af25161418ebadd33b8cf95d0325
anavianm <anavian.max@gmail.com>
2020-11-17 17:52:48 -0500

    tried to add some sounds

---
+++ Assets/PickUpObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+
+    public static AudioClip objectPickUpSound;
+    static AudioSource audioSrc;
+    // Start is called before the first frame update
+    void Start () 
+    {
+        objectPickUpSound = Resources.Load<AudioClip>("PickUpSound");
+
+        audioSrc = GetComponent<AudioSource> ();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {   
+        if(other.gameObject.tag.Equals("Player")){
+                audioSrc.PlayOneShot (objectPickUpSound);
+        }     
+       
+    }
+}
