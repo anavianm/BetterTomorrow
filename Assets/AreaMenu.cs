c18eae2d1dbbcfd2f1cbf8f842501b50e562bbac
anavianm <anavian.max@gmail.com>
2020-12-09 01:13:38 -0500

    some more anim

--- Assets/AreaMenu.cs
+++ Assets/AreaMenu.cs
@@ -8,7 +8,7 @@ public class AreaMenu : MonoBehaviour
     public void GoGame()
     {
         // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(4);
     }
 
     void Start() {
